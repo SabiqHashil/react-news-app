9e0f682cabc0d21fd04cf5e5d4f2e4b812bbcb27
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2024-08-29 14:32:51 +0530

    added folders and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-    <>
-      <h1>Welcome to SBQ solutions</h1>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Container from "../Container/Container";
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Home page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi omnis,
+        exercitationem error quos corrupti quidem porro laboriosam consequuntur
+        cupiditate natus facilis quia magnam? Error, neque.
+      </p>
+      <a href="">Link</a>
+    </Container>
+  );
+};
+
+export default Home;
